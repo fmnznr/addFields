36ad863983b52092330b3972987d9f9754482ec2
Franz Münzner <franzmuenzner@MBP-von-Franz.fritz.box>
2025-01-14 13:41:35 +0100

    a

--- src/ContaoManager/plugin.php
+++ src/ContaoManager/plugin.php
@@ -1,20 +1,28 @@
 <?php
 
-namespace Mhe\AddFields\ContaoManager;
+namespace fmnznr\AddFields\ContaoManager;
 
 use Contao\CoreBundle\ContaoCoreBundle;
-use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
-// ggf. weitere Interfaces wie BundleDependenciesInterface
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Vendor\SomeBundle\SomeBundle;
 
 class Plugin implements BundlePluginInterface
 {
-    public function getBundles(BundleConfig $config)
+    public function getBundles(ParserInterface $parser)
     {
-        // Beispiel:
         return [
-            BundleConfig::create(\Mhe\AddFields\MheAddFieldsBundle::class)
-                ->setLoadAfter([ContaoCoreBundle::class]),
+            BundleConfig::create(SomeBundle::class)
+                // This loads the bundle after the ContaoCorebundle
+                // and after the legacy module called "notification_center".
+                ->setLoadAfter([
+                    ContaoCoreBundle::class,
+                    'notification_center'
+                ]),
+                // This will replace the legacy module called "old_module_name"
+                // with SomeBundle in the plugin loader.
+                ->setReplace(['old_module_name']),
         ];
     }
 }
